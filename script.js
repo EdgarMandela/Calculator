e29ce7b1376122f75bb1987b92ee41ae1986b68c
EdgarMandela <mandela0edgar@gmail.com>
2019-11-28 11:50:36 +0300

    added

--- script.js
+++ script.js
@@ -1,6 +1,41 @@
-var add = function (numb1,numb2){
-    return numb1 + numb2;
+var add = function(number1, number2) {
+    return number1 + number2;
+  };
+  
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var resultAdd = add(number1, number2);
+  alert(resultAdd);
+
+  var subract = function(number1, number2) {
+    return number1 - number2;
+  };
+  
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var resultSubtract = subract(number1, number2);
+  alert(resultSubtract);
+
+  var multiply = function(number1, number2) {
+    return number1 * number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var resultMultiply = multiply(number1, number2);
+  alert(resultMultiply);
+
+  var divide = function(number1, number2) {
+    return number1 / number2;
+  };
+  var number1 = parseInt(prompt("Enter a number:"));
+  var number2 = parseInt(prompt("Enter another number:"));
+  var resultDivide = divide(number1, number2);
+  alert(resultDivide);
+
+  var bodyMassindex = function (weight,height){
+    return weight / height;
     };
-    var numb1 = parseInt(prompt ("Enter a number"));
-    var numb2 = parseInt(prompt ("Enter the second number"));
-    alert(add(numb1,numb2));
+    var weight = parseInt(prompt("enter your weight"));
+    var height = parseInt(prompt("enter your height"));
+    var calculate = bodyMassindex(weight,height);
+    alert(calculate);
