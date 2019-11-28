0ee96e98c8c78d24056304b715b65aebf7ebc0f3
EdgarMandela <mandela0edgar@gmail.com>
2019-11-28 11:03:12 +0300

    created js file

---
+++ script.js
@@ -0,0 +1,6 @@
+var add = function (numb1,numb2){
+    return numb1 + numb2;
+    };
+    var numb1 = parseInt(prompt ("Enter a number"));
+    var numb2 = parseInt(prompt ("Enter the second number"));
+    alert(add(numb1,numb2));
